0088a138811dca28397d8dec6bdd329a4651ad82
caiotelesz <caioteles1267@gmail.com>
2024-10-30 11:53:03 -0300

    config and creating page

---
+++ README.md
@@ -0,0 +1,1 @@
+# Tulfa Test
---
+++ src/pages/App.tsx
@@ -0,0 +1,22 @@
+import './style.scss';
+
+function App() {
+
+  return (
+    <div>
+      <section className='section2'>
+      <h2>Tulfa AR creates immersive and engaging eCommerce experiences</h2>
+
+      <div>
+        <img src= "/assets/images/nike-image.png" alt="nike" />
+        <img src= "/assets/images/barn-image.png" alt="barn" />
+        <img src= "/assets/images/MLB-image.png" alt="MLB" />
+        <img src= "/assets/images/boomChicka-image.png" alt="BoomChicka" />
+        <img src= "/assets/images/MHD-image.png" alt="MHD" />
+      </div>
+    </section>
+    </div>
+  )
+}
+
+export default App;
