d18ae29186f73623ea5219ed3f57c1acf241a32b
caiotelesz <caioteles1267@gmail.com>
2024-10-30 22:35:44 -0300

    Video button

--- src/components/mediaDisplay/index.tsx
+++ src/components/mediaDisplay/index.tsx
@@ -11,27 +11,39 @@ interface MediaDisplayProps {
   mediaType: 'image' | 'video';
 }
 
-
-
 export function MediaDisplay({ mediaSrc, mediaType }: MediaDisplayProps) {
-
   const videoRef = useRef<HTMLVideoElement>(null);
   const [play, setPlay] = useState(false);
+  const [showButton, setShowButton] = useState(true);
 
   function handlePlayVideo() {
-    if (!videoRef.current) return
+    if (!videoRef.current) return;
     if (videoRef.current.paused) {
       setPlay(true);
       videoRef.current.play();
+      setShowButton(false);
     } else {
       setPlay(false);
       videoRef.current.pause();
+      setShowButton(true);
     }
   }
 
-  return(
-    <div className='media-container'>
-      <div className='media-info'>
+  function handleMouseEnter() {
+    if (play) {
+      setShowButton(true);
+    }
+  }
+
+  function handleMouseLeave() {
+    if (play) {
+      setShowButton(false);
+    }
+  }
+
+  return (
+    <div className="media-container">
+      <div className="media-info">
         <h2>Metaverse Content Production</h2>
         <h3>InStore Immersive Activation</h3>
         <p>Tulfa’s AR feature was tailor-made to suit both the venue and the audience. With the easy-to-operate touchscreen scan interface, we brought live-action bees to Kate Spade customers!</p>
@@ -39,14 +51,21 @@ export function MediaDisplay({ mediaSrc, mediaType }: MediaDisplayProps) {
         <a href="#">Learn more about ar</a>
       </div>
 
-      <div className='media-content'>
-        {mediaType == 'image' ? (
-          <img src={mediaSrc} alt="Media Content" className='media-image'/>
+      <div className="media-content">
+        {mediaType === 'image' ? (
+          <img src={mediaSrc} alt="Media Content" className="media-image" />
         ) : (
-          <div className="media-video">
-            <button onClick={handlePlayVideo}>{!play ? ( <Play fill='rgba(0, 0, 0, 0.25)' /> ) : ( <Pause fill='rgba(0, 0, 0, 0.25)'/> )}</button>
-
-            <video src={mediaSrc} ref={videoRef} >
+          <div
+            className="media-video"
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+          >
+            {showButton && (
+              <button onClick={handlePlayVideo}>
+                {!play ? <Play fill="rgba(0, 0, 0, 0.5)" /> : <Pause fill="rgba(0, 0, 0, 0.5)" />}
+              </button>
+            )}
+            <video src={mediaSrc} ref={videoRef} width="500" height="280">
               <source src="/assets/videos/product-video.mp4" type="video/mp4" />
               <track
                 src="/assets/videos/product-video.mp4"
